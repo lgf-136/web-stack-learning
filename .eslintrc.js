1b57c837382fc0c58af364ae803a1cd596e82729
lgf <lgf_136@163.com>
2022-09-25 17:55:06 +0800

    feat: 🎸 init
    init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  extends: [ './config/eslint/typescript.config' ],
+  rules: {
+    complexity: [ 'error', { max: 20 } ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxBOF: 1,
+        maxEOF: 1,
+      },
+    ],
+  },
+  settings: {},
+  plugins: [],
+  overrides: [],
+  ignorePatterns: [ 'src/**/*.d.ts' ],
+};
+
